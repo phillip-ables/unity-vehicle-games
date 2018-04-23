fc15a066474a6349b6a217d68d1d10b210048d52
lip ables <phillipables1@gmail.com>
2018-04-23 15:31:57 -0400

    on update we can rotate our plane by our mouse input axis

--- FlightTerrain/Assets/PlanePilot.cs
+++ FlightTerrain/Assets/PlanePilot.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlanePilot : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        Debug.Log("Plane Pilot Script added to:" + gameObject.name);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+    }
 }
