d6795a99abc6798fb22208062f3f03ad8a817de9
lip ables <phillipables1@gmail.com>
2018-04-24 17:07:35 -0400

    require component camera, public settings of Shader Transform rfloat for aspect ratio and float radius, private variable type Camera for cam and _material for something we will be instantiated in game

--- FlightTerrain/Assets/BlackHoleShadder.cs
+++ FlightTerrain/Assets/BlackHoleShadder.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// when we are attaching this to an object their is a camera there
+[RequireComponent(typeof(Camera))]
 public class BlackHoleShadder : MonoBehaviour {
+    //public settings
+    public Shader shader;
+    public Transform blackhole;
+    public float ratio;  // aspect ratio of the screen;
+    public float radius;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //private settings
+    Camera cam;
+    Material _material;  // this isnt something that exist means underscore
+
+
+
 }
