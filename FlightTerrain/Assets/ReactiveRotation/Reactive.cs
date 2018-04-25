de423b20901b078d9bf0ea43eff49f812338efa9
lip ables <phillipables1@gmail.com>
2018-04-24 21:45:54 -0400

    new script created called Reactive, public variables for angle range and a boolean for inverse x and y, private vector2s for mouse pos and base rotation

--- FlightTerrain/Assets/ReactiveRotation/Reactive.cs
+++ FlightTerrain/Assets/ReactiveRotation/Reactive.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Reactive : MonoBehaviour {
+    public Vector2 angleRange = new Vector2(4f, 4f);
+    public bool inverseX;
+    public bool inverseY;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Vector2 mousePos;
+    private Vector2 baseRotation;
+
+    
 }
